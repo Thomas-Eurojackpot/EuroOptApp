b23f06211cfd3e58ee3138e66c1ea80edad330fe
Thomas-Eurojackpot <menke.th@t-online.de>
2026-08-02 13:47:31 +0200

    Initial Commit

---
+++ EuroOpt/EuroOptApp.swift
@@ -0,0 +1,17 @@
+//
+//  EuroOptApp.swift
+//  EuroOpt
+//
+//  Created by Thomas Menke on 02.08.26.
+//
+
+import SwiftUI
+
+@main
+struct EuroOptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
