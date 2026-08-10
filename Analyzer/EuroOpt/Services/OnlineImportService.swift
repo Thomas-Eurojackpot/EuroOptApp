336edfd31ac4c7ebe7602c40c6dc55545c0570bc
Thomas-Eurojackpot <menke.th@t-online.de>
2026-08-10 17:01:54 +0200

    Fix online Eurojackpot import request handling

--- Analyzer/EuroOpt/Services/OnlineImportService.swift
+++ Analyzer/EuroOpt/Services/OnlineImportService.swift
@@ -2,28 +2,37 @@ import Foundation
 
 final class OnlineImportService {
 
-    func downloadLatestDraws(completion: @escaping (Result<Data, Error>) -> Void) {
+    private let url = URL(string: "https://www.lotto.de/bin/eurojackpot.json")!
 
-        guard let url = URL(string: "https://www.lotto.de/bin/eurojackpot.json") else {
-            return
-        }
+    func downloadLatestDraws(
+        completion: @escaping (Result<Data, Error>) -> Void
+    ) {
 
-        URLSession.shared.dataTask(with: url) { data, response, error in
+        var request = URLRequest(url: url)
+        request.timeoutInterval = 15
+        request.cachePolicy = .reloadIgnoringLocalCacheData
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
 
-            if let error = error {
+        URLSession.shared.dataTask(with: request) { data, response, error in
+
+            if let error {
                 completion(.failure(error))
                 return
             }
 
-            guard let data = data else {
+            guard let http = response as? HTTPURLResponse,
+                  (200...299).contains(http.statusCode) else {
                 completion(.failure(URLError(.badServerResponse)))
+                return
+            }
+
+            guard let data, !data.isEmpty else {
+                completion(.failure(URLError(.zeroByteResource)))
                 return
             }
 
             completion(.success(data))
 
         }.resume()
-
     }
-
 }
